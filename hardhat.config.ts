3689f4e1a2906fb5120da1f2cd5439f745066dba
Johnson Oyemade <johnsonoca@gmail.com>
2026-03-22 17:06:07 +0100

    feat: deployed and verified token contract on etherscan

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomiclabs/hardhat-ethers";
-import "@nomiclabs/hardhat-etherscan";
+import "@nomicfoundation/hardhat-verify";
 import "@typechain/hardhat";
 import "@nomicfoundation/hardhat-chai-matchers";
 import * as dotenv from "dotenv";
